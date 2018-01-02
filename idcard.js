9e8bee43d4efc8a6334d07dd2f9b24974ffe49da
Gabriel Flores <gabriel330flores@gmail.com>
2018-01-02 13:45:26 -0600

    Added a function to post all values from the inputs

---
+++ idcard.js
@@ -0,0 +1,25 @@
+function = idCard(){
+  var firstName = document.getElementById('firstName').value;
+  var lastName = document.getElementById('lastName').value;
+  var address = document.getElementById('address').value;
+  var postFullName = firstName + lastName;
+  document.getElementById('postFullName').innerHTML = postFullName;
+
+  var postAddress = address
+  document.getElementById('postAddress').innerHTML = postAddress;
+
+  var age = document.getElementById('age').value;
+  var phoneNumber = document.getElementById('phoneNumber').value;
+
+  var numberArray = []{
+    age.push
+    phoneNumber.push
+    for (var i = 0; i < numberArray; i++) {
+      if (numberArray[i] <= 100){
+        document.getElementById('age').innerHTML;
+      } else (numberArray[i] > 100){
+        document.getElementById('phoneNumber').innerHTML;
+      }
+    }
+  }
+}
