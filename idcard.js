2463b0aa644faa946f5304db3c7372e6e68e6c92
Gabriel Flores <gabriel330flores@gmail.com>
2018-01-02 17:05:58 -0600

    Fixed all errors

--- idcard.js
+++ idcard.js
@@ -2,7 +2,7 @@ function idCard(){
   var firstName = document.getElementById('firstName').value;
   var lastName = document.getElementById('lastName').value;
   var address = document.getElementById('address').value;
-  var postFullName = firstName + lastName;
+  var postFullName = firstName + ' ' + lastName;
   document.getElementById('postFullName').innerHTML = postFullName;
 
   var postAddress = address
@@ -12,13 +12,12 @@ function idCard(){
   var phoneNumber = document.getElementById('phoneNumber').value;
 
   var numberArray = [];
-    age.push
-    phoneNumber.push
-    for (var i = 0; i < numberArray; i++) {
+    numberArray.push(age, phoneNumber);
+    for (var i = 0; i < numberArray.length; i++) {
       if (numberArray[i] <= 100){
-        document.getElementById('age').innerHTML;
-      } else (numberArray[i] > 100){
-        document.getElementById('phoneNumber').innerHTML;
+        document.getElementById('postAge').innerHTML = 'Age:' + age;
+      } else (numberArray[i] > 100);{
+        document.getElementById('postPhoneNumber').innerHTML ='Phone Number:' + phoneNumber;
     }
   }
 }
